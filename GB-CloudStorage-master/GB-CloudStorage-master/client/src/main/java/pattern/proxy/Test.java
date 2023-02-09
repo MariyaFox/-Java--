1d84620c4fb8d161e9ff02b1896b2a9ffd516d50
MF <ms.delo000@mail.ru>
2023-02-09 22:52:31 +0700

    Task4

---
+++ GB-CloudStorage-master/GB-CloudStorage-master/client/src/main/java/pattern/proxy/Test.java
@@ -0,0 +1,13 @@
+package pattern.proxy;
+
+public class Test {
+    public static void main(String[] args) {
+        LongGetter getter = new LongGetter() {};
+
+        LongGetter proxy = new LongGetterCached(getter) {};
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(proxy.getLong(100));
+        }
+    }
+}
